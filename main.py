03c4b138a022a079ea45783b21168f4813c5f4c6
Ismail Gadzhiev <ismailgadzhievff@gmail.com>
2023-06-01 11:41:58 +0300

    add readme

--- main.py
+++ main.py
@@ -4,9 +4,4 @@ import logging
 logger = logging.getLogger()
 
 if __name__ == "__main__":
-    with open("./result/b5fcb4ad-6b42-4021-aefb-a030e662aacc/content.html", "r") as file:
-        content = file.read()
-      
-    soup  = BeautifulSoup(content, "html.parser")
-
-    print(len(soup.find_all("div", class_="Hotels__Wrapper")))
+    print(1)
