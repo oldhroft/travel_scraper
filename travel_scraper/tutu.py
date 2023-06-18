fd776f493e461666125881b935ed869313252b05
GOLoDovkA-A <mr.andrey.golda@mail.ru>
2023-06-18 23:13:01 +0100

    with new dates grid

--- travel_scraper/tutu.py
+++ travel_scraper/tutu.py
@@ -21,6 +21,8 @@ def parse_with_params(
         *,
         arrival_country_id,
         departure_country_id,
+        date_begin,
+        date_end,
         min_nights,
         max_nights,
         adults,
@@ -30,10 +32,6 @@ def parse_with_params(
         sleep_between_scroll: int = 8
 ):
     stat = {}
-    fmt = "%d.%m.%Y"
-    today = datetime.date.today()
-    date_begin = (today + datetime.timedelta(days=1)).strftime(fmt)
-    date_end = (today + datetime.timedelta(days=31)).strftime(fmt)
 
     url = TEMPLATE_URL.format(
         arrival_country_id=arrival_country_id,
@@ -60,13 +58,11 @@ def parse_with_params(
         content = browser.page_source
         return content, stat
 
-    height_diff = 1
     one_link_counter = 0
 
-    # main scrapping loop
-    while height_diff > 0:
+    # main scrolling loop
+    while one_link_counter < times_scrolled:
         try:
-            height_diff = return_height(browser)
             next_button_element = browser.find_elements("xpath", "//*[contains(text(), 'Показать больше предложений')]")[2]
             browser.execute_script("arguments[0].scrollIntoView();", next_button_element)
             webdriver.ActionChains(browser).move_to_element(next_button_element).click(next_button_element).perform()
@@ -80,7 +76,6 @@ def parse_with_params(
         logger.info("Successful iteration -> sleeping between scroll")
         time.sleep(sleep_between_scroll)
 
-        height_diff = return_height(browser) - height_diff
         one_link_counter += 1
 
     logger.info(f"Scrolling finished with amount of iterations: {one_link_counter}")
@@ -89,22 +84,30 @@ def parse_with_params(
     return content, stat
 
 
-def grid_option(grid_config: dict, night_interval: int = 0) -> list:
+def grid_option(grid_config: dict, times_scrolled, night_interval: int = 0, days_from: int = 2,
+                days_to: int = 30) -> list:
     if night_interval < 0:
         raise ValueError("Night interval should be > 0")
+
+    start_date = datetime.datetime.now() + datetime.timedelta(days=days_from)
+    date_grid = [start_date + datetime.timedelta(days=x) for x in range(0, days_to)]
+    date_grid = [t.strftime('%d.%m.%Y') for t in date_grid]
+    grid_config['dates'] = date_grid
 
     grid = create_grid(grid_config)
 
     return list(
         map(
             lambda x: {
-                "departure_country_id": x["departure_country_id"],
                 "arrival_country_id": x["arrival_country_id"],
+                "departure_country_id": x["departure_country_id"],
+                'date_begin': x['dates'],
+                'date_end': x['dates'],
                 "adults": x["adults"],
                 "children": x["children"],
                 "min_nights": x["nights"],
                 "max_nights": x["nights"] + night_interval,
-                "times_scrolled": x["times_scrolled"],
+                "times_scrolled": times_scrolled,
             },
             grid,
         )
